1ee680d3b1a50de2f656d7e39d56cd04caddde23
Christopher EnyTC <chris@enytc.com>
2016-07-23 09:57:39 -0300

    Fix travis build

--- bin/travis.sh
+++ bin/travis.sh
@@ -2,7 +2,7 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb +extension RANDR start
+  sh -e /etc/init.d/xvfb start +extension RANDR
   sleep 3
 fi
 
