ac44a68f190e698d27e804beffe92db968c52412
Christopher EnyTC <chris@enytc.com>
2016-07-23 20:22:46 -0300

    Rename binary test script

---
+++ bin/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start +extension RANDR
+  sleep 3
+fi
+
+NODE_EXEC_PATH=$(which node)
+
+export NODE_PATH=$NODE_EXEC_PATH
+
+./node_modules/.bin/electron-mocha build --ui bdd --timeout 80000 --reporter spec --inline-diffs --bail build
--- bin/travis.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start +extension RANDR
-  sleep 3
-fi
-
-npm test
