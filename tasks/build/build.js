c02e920103fa70a2e638e387ad30280f8205b20f
Christopher EnyTC <chris@enytc.com>
2016-07-23 08:00:31 -0300

    Add tasks

---
+++ tasks/build/build.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var pathUtil = require('path');
+var Q = require('q');
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var plumber = require('gulp-plumber');
+var jetpack = require('fs-jetpack');
+
+var bundle = require('./bundle');
+var generateSpecImportsFile = require('./generate_spec_imports');
+var utils = require('../utils');
+
+var projectDir = jetpack;
+var srcDir = projectDir.cwd('./app');
+var destDir = projectDir.cwd('./build');
+
+var paths = {
+    copyFromAppDir: [
+        './node_modules/**',
+        './helpers/**',
+        './assets/**',
+        './**/*.html',
+        './**/*.+(jpg|png|svg)'
+    ],
+};
+
+// -------------------------------------
+// Tasks
+// -------------------------------------
+
+gulp.task('clean', function () {
+    return destDir.dirAsync('.', { empty: true });
+});
+
+
+var copyTask = function () {
+    return projectDir.copyAsync('app', destDir.path(), {
+            overwrite: true,
+            matching: paths.copyFromAppDir
+        });
+};
+gulp.task('copy', ['clean'], copyTask);
+gulp.task('copy-watch', copyTask);
+
+
+var bundleApplication = function () {
+    return Q.all([
+            bundle(srcDir.path('background.js'), destDir.path('background.js')),
+            bundle(srcDir.path('app.js'), destDir.path('app.js')),
+            bundle(srcDir.path('src/services/uber.factory.js'), destDir.path('src/services/uber.factory.js')),
+            bundle(srcDir.path('src/controllers/auth.ctrl.js'), destDir.path('src/controllers/auth.ctrl.js')),
+            bundle(srcDir.path('src/controllers/success.ctrl.js'), destDir.path('src/controllers/success.ctrl.js')),
+        ]);
+};
+
+var bundleSpecs = function () {
+    return generateSpecImportsFile().then(function (specEntryPointPath) {
+        return bundle(specEntryPointPath, destDir.path('spec.js'));
+    });
+};
+
+var bundleTask = function () {
+    if (utils.getEnvName() === 'test') {
+        return bundleSpecs();
+    }
+    return bundleApplication();
+};
+
+gulp.task('bundle', ['clean'], bundleTask);
+gulp.task('bundle-watch', bundleTask);
+
+gulp.task('environment', ['clean'], function () {
+    var configFile = 'config/env_' + utils.getEnvName() + '.json';
+    projectDir.copy(configFile, destDir.path('env.json'));
+});
+
+
+gulp.task('package-json', ['clean'], function () {
+    var manifest = srcDir.read('package.json', 'json');
+
+    // Add "dev" suffix to name, so Electron will write all data like cookies
+    // and localStorage in separate places for production and development.
+    if (utils.getEnvName() === 'development') {
+        manifest.name += '-dev';
+        manifest.productName += ' Dev';
+    }
+
+    destDir.write('package.json', manifest);
+});
+
+
+gulp.task('watch', function () {
+    watch('app/**/*.js', batch(function (events, done) {
+        gulp.start('bundle-watch', done);
+    }));
+    watch(paths.copyFromAppDir, { cwd: 'app' }, batch(function (events, done) {
+        gulp.start('copy-watch', done);
+    }));
+});
+
+
+gulp.task('build', ['bundle', 'copy', 'environment', 'package-json']);
