69e263544a9c869122264ecf6be703b8802edeb7
Christopher EnyTC <chris@enytc.com>
2016-07-24 16:38:17 -0300

    Fix relative path on keychain

--- script/keychain.sh
+++ script/keychain.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    export CERTIFICATE_P12=Certificate.p12;
-    echo "$CERTIFICATE_OSX_P12" | base64 — decode > $CERTIFICATE_P12;
+    export CERTIFICATE_P12=${TRAVIS_BUILD_DIR}/Certificate.p12;
+    echo "$CERTIFICATE_OSX_P12" | base64 — decode > "$CERTIFICATE_P12";
     export KEYCHAIN=build.keychain;
     security create-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN;
     security default-keychain -s $KEYCHAIN;
     security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN;
-    security import $CERTIFICATE_P12 -k $KEYCHAIN -P "$CERTIFICATE_PASSWORD" -T /usr/bin/codesign;
+    security import "$CERTIFICATE_P12" -k $KEYCHAIN -P "$CERTIFICATE_PASSWORD" -T /usr/bin/codesign;
 fi
