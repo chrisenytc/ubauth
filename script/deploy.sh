05e334cf6808eb0322306fbfaa640503fe1a1104
Christopher EnyTC <chris@enytc.com>
2016-07-24 12:53:35 -0300

    Update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,6 +8,10 @@ VERSION=$(echo "$PKG" | jq '.version')
 
 CLEANED_VERSION=${VERSION//\"}
 
-export APP_VERSION="$CLEANED_VERSION"
+export APP_VERSION=${CLEANED_VERSION}
 
-echo "Deploying version '$APP_VERSION' to GitHub releases. ¯\\_(ツ)_/¯"
+echo "Deploying version ${APP_VERSION} to GitHub releases. ¯\\_(ツ)_/¯"
+
+export RELEASED_PACKAGE_FILENAME="${TRAVIS_BUILD_DIR}/releases/ubauth-${APP_VERSION}-darwin-x64.dmg"
+
+echo "Package file: ${RELEASED_PACKAGE_FILENAME}"
