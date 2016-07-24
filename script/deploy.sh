969b8f8387afbed966f1f5aff1fcf3367cd2e984
Christopher EnyTC <chris@enytc.com>
2016-07-24 11:45:01 -0300

    Update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-npm run release -- --sign "$CODESIGN_KEY"
+#npm run release -- --sign "$CODESIGN_KEY"
+
+PKG=$(cat ./app/package.json)
+
+VERSION=$(echo "$PKG" | jq '.version')
+
+CLEANED_VERSION=${VERSION//\"}
+
+export APP_VERSION="$CLEANED_VERSION"
+
+echo "Deploying version '$APP_VERSION' to GitHub releases. ¯\\_(ツ)_/¯"
