7c9a05ef1bf8a9fcda5b103302e50caa347dfe02
Christopher EnyTC <chris@enytc.com>
2016-07-24 14:36:52 -0300

    Update test script

--- script/test.sh
+++ script/test.sh
@@ -4,4 +4,4 @@ NODE_EXEC_PATH=$(which node)
 
 export NODE_PATH=$NODE_EXEC_PATH
 
-./node_modules/.bin/electron-mocha build --ui bdd --timeout 80000 --reporter spec --inline-diffs --bail build
+"$TRAVIS_BUILD_DIR/node_modules/.bin/electron-mocha build --ui bdd --timeout 80000 --reporter spec --inline-diffs --bail $TRAVIS_BUILD_DIR/build"
