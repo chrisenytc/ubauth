404b352dc87a7d9c78ba89da3e0c2e8f4e5cb4c7
Christopher EnyTC <chris@enytc.com>
2016-07-24 05:13:10 -0300

    Update bootstrap test

--- app/test/bootstrap.spec.js
+++ app/test/bootstrap.spec.js
@@ -1,14 +1,50 @@
 import './test_helper';
 
-describe("Bootstrap", function () {
-    it('should open the app successfully', function () {
-        return this.app.client.waitUntilWindowLoaded()
-            .getWindowCount().should.eventually.equal(1)
-            .browserWindow.isMinimized().should.eventually.be.false
-            .browserWindow.isDevToolsOpened().should.eventually.be.false
-            .browserWindow.isVisible().should.eventually.be.true
-            .browserWindow.isFocused().should.eventually.be.true
-            .browserWindow.getBounds().should.eventually.have.property('width').and.be.above(0)
-            .browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0);
+let pkg = require('./package.json');
+
+describe("Bootstrap", function() {
+
+    describe('#MainWindow', function() {
+
+        beforeEach(function() {
+            return this.app.client.waitUntilWindowLoaded();
+        });
+
+        it('should open only 1', function() {
+            return this.app.client.getWindowCount()
+                .should.eventually.equal(1);
+        });
+
+        it('should not be minimized', function() {
+            return this.app.client.browserWindow.isMinimized()
+                .should.eventually.be.false;
+        });
+
+        it('should not open the DevTools window', function() {
+            return this.app.client.browserWindow.isDevToolsOpened()
+                .should.eventually.be.false;
+        });
+
+        it('should be visiable', function() {
+            return this.app.client.browserWindow.isVisible()
+                .should.eventually.be.true;
+        });
+
+        it('should be focused', function() {
+            return this.app.client.browserWindow.isFocused()
+                .should.eventually.be.true;
+        });
+
+        it('should have width equals to \'350px\' and height equals to \'830\'', function() {
+            return this.app.client
+                .browserWindow.getBounds().should.eventually.have.property('width').and.be.equal(350)
+                .browserWindow.getBounds().should.eventually.have.property('height').and.be.equal(830)
+        });
+
+        it('should have title equals to \'' + pkg.productName + '\'', function() {
+            return this.app.client.browserWindow.getTitle()
+                .should.eventually.be.equal(pkg.productName);
+        });
+
     });
 });
