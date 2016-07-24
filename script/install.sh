1ddf585ac35c48ae5a9d74e2f124280472b27451
Christopher EnyTC <chris@enytc.com>
2016-07-24 14:09:41 -0300

    Update install script

--- script/install.sh
+++ script/install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS" != true ]]; then
-    bundle install
-    overcommit --install
+if [[ "$TRAVIS" == true ]]; then
+  NVM_ROOT="${HOME}/.nvm"
+  rm -rf "$NVM_ROOT"
+  git clone https://github.com/creationix/nvm.git "$NVM_ROOT"
+  # shellcheck disable=SC1090
+  source "${NVM_ROOT}/nvm.sh"
+  nvm install "$NODE_VERSION"
+  node --version
+  npm --version
+else
+  bundle install
+  overcommit --install
 fi
+
 
  cd app || exit
 
