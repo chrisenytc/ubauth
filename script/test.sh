bddaed5688282ca743721eae26998fbe1f4a0685
Christopher EnyTC <chris@enytc.com>
2016-07-24 10:17:05 -0300

    Update test script

--- script/test.sh
+++ script/test.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export DISPLAY=":99.0"
-  export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
-  sh -e /etc/init.d/xvfb start +extension RANDR
-  sleep 8
-fi
-
 NODE_EXEC_PATH=$(which node)
 
 export NODE_PATH=$NODE_EXEC_PATH
