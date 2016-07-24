4fc757ddf301ab657c7143d60ab432ca3323b34f
Christopher EnyTC <chris@enytc.com>
2016-07-24 04:04:30 -0300

    Add test helper

---
+++ app/test/test_helper.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import electron from 'electron-prebuilt';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { Application } from 'spectron';
+
+chai.use(chaiAsPromised);
+
+chai.should();
+
+beforeEach(function () {
+    let appPath = path.join(__dirname, '..', 'build');
+    this.app = new Application({
+        path: electron,
+        args: [appPath],
+        startTimeout: 8000,
+        chromeDriverLogPath: '/tmp/chromedriver.log',
+        nodePath: process.env.NODE_PATH
+    })
+    return this.app.start()
+});
+
+beforeEach(function () {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness
+});
+
+afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+        return this.app.stop()
+    }
+});
